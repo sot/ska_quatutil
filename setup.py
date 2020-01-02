0a5d298e3e2f38c19567f4ce6fed24787c919f0f
Tom Aldcroft <taldcroft@gmail.com>
2020-01-02 10:54:24 -0500

    Update author email

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 setup(name='Ska.quatutil',
       author='Tom Aldcroft',
       description='ACA quaternion utilities',
-      author_email='aldcroft@head.cfa.harvard.edu',
+      author_email='taldcroft@cfa.harvard.edu',
       url='http://cxc.harvard.edu/mta/ASPECT/tool_doc/pydocs/Ska.quatutil.html',
       version=__version__,
       zip_safe=False,
